046f7d1271afa0b3ecd5612f4af0127e53ee70c9
Sébastien <seb.lasante@hotmail.com>
2021-01-04 08:29:43 +0100

    fini

---
+++ exo_5_array/public/js/main.js
@@ -0,0 +1,39 @@
+// EXERCICE 1
+
+let tabExo = ["pomme", "sucre",2,4,3==3];
+console.log(tabExo);
+
+// EXERCICE 2
+
+console.log(tabExo.length);
+
+// EXERCICE 3
+
+tabExo.pop();
+console.log(tabExo);
+
+// EXERCICE 4
+
+tabExo.shift();
+console.log(tabExo);
+
+// EXERCICE 5
+
+tabExo.push("cerise")
+tabExo.push("canne")
+console.log(tabExo);
+
+// EXERCICE 6
+
+tabExo.unshift("pizza")
+tabExo.unshift("tacos")
+tabExo.unshift("maki")
+console.log(tabExo);
+
+// EXERCICE 7
+
+tabExo = tabExo.reverse();
+console.log(tabExo);
+// [21:20]
+
+// [21:21]
